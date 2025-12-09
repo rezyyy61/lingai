f0a5943bb8e413e556ec1464b5b14b791ef6f2f1
rezyy <rezyyy61@gmail.com>
2025-12-09 16:34:18 +0100

    fix: use /api as default API base URL

--- config/cors.php
+++ config/cors.php
@@ -28,6 +28,8 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'https://lingai.nl',
+        'https://www.lingai.nl',
     ],
 
     'allowed_origins_patterns' => [],
